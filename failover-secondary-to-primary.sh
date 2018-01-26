142dfc5f2041c5a9ed49b0f9cab1c196a25e9390
David Blundell <david.blundell@100percentit.com>
2018-01-26 12:03:29 +0000

    Update failover-secondary-to-primary.sh

--- failover-secondary-to-primary.sh
+++ failover-secondary-to-primary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # failover-secondary-to-primary.sh
-neutron --os-cloud 100percentit floatingip-associate [floatingip-ID] [vrrp-secondary port ID]
-neutron --os-cloud 100percentit floatingip-disassociate [floatingip-ID] [vrrp-primary port ID]
+neutron --os-cloud 100percentit floatingip-disassociate [floatingip-ID] [vrrp-secondary port ID]
+neutron --os-cloud 100percentit floatingip-associate [floatingip-ID] [vrrp-primary port ID]
